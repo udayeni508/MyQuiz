5a0eb0601a7217e280e40a1b3e011bee4f8ca1a0
udayeni508 <udayeni.123@gmail.com>
2019-05-16 16:00:13 -0400

    Initial commit

---
+++ app/src/main/java/com/example/admin/myquiz/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.admin.myquiz;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button buttonStartQuiz = findViewById(R.id.button_start_quiz);
+        buttonStartQuiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startQuiz();
+            }
+        });
+    }
+
+    private void startQuiz() {
+        Intent intent = new Intent(MainActivity.this, QuizActivity.class);
+        startActivity(intent);
+    }
+    }
+}
